634364a32b95a87e89d8fdb74198498eb23d2584
Lynnette_jk <lynnettecheruto@gmail.com>
2023-03-11 21:28:39 +0300

    exercise 3.3

--- index.js
+++ index.js
@@ -43,6 +43,17 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${count} people. ${timestamp} ${timezone} (GMT${offset >= 0 ? '+' : '-'}${Math.abs(offset)})`);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
